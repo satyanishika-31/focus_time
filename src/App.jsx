e1ad4958db68f2fe6b24d7771e5e0ebb010b96ca
satyanishika-31 <satyanishika-31@gmail.com>
2026-06-15 15:16:28 +0530

    Initial commit of focus_time project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState ,useEffect} from "react";
+import FocusTimer from "./Components/FocusTimer";
+
+import React from 'react'
+
+function App() {
+  
+ const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const saved = localStorage.getItem("theme");
+    if (saved) setTheme(saved);
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <div className={`min-h-screen ${theme === "light" ? "bg-white text-black" : "bg-gray-900 text-white"}`}>
+      <button
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className="m-4 px-2 py-2 bg-cyan-400 text-white rounded-2xl"
+      >
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
+      </button>
+      <FocusTimer />
+    </div>
+  );
+
+}
+
+export default App
+  
---
+++ src/Components/FocusTimer.jsx
@@ -0,0 +1,62 @@
+
+import React, { useState, useEffect } from "react";
+
+function FocusTimer() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => setTime((t) => t + 1), 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const startTimer = () => setIsRunning(true);
+  const stopTimer = () => {
+    setIsRunning(false);
+    if (time > 0) {
+      const session = {
+        duration: time,
+        completedAt: new Date().toLocaleString(),
+      };
+      setSessions([...sessions, session]);
+    }
+  };
+  const resetTimer = () => setTime(0);
+
+  const formatTime = (seconds) =>
+    new Date(seconds * 1000).toISOString().substr(11, 8);
+
+  return (
+    <div>
+
+    <div className="p-6 text-center m-10 ">
+      <h1 className="text-2xl font-bold">Focus Timer</h1>
+      <p className="text-4xl my-4 border-1 p-3">{formatTime(time)}</p>
+      <div className="space-x-2">
+        <button onClick={startTimer} className="px-4 py-2 bg-green-500 text-white rounded">Start</button>
+        <button onClick={stopTimer} className="px-4 py-2 bg-red-500 text-white rounded">Stop</button>
+        <button onClick={resetTimer} className="px-4 py-2 bg-gray-500 text-white rounded">Reset</button>
+      </div>
+    </div>
+    <div>
+      <div className="p-6 text-center m-10">
+
+      <h2 className="mt-6 text-lg font-semibold">Session History</h2>
+      <ul className="mt-2">
+        {sessions.map((s, i) => (
+          <li key={i} className="border p-2 my-1">
+            {formatTime(s.duration)} – {s.completedAt}
+          </li>
+        ))}
+      </ul>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default FocusTimer;
